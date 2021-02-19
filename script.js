89f9c4fc3717591bdd82251019f4e4ba807502bc
Aamer Desai <56724460+minm333@users.noreply.github.com>
2021-02-19 18:58:31 -0500

    Add functionality to to display today, yesterday, x days ago upto 7days on transactions for better UX

--- script.js
+++ script.js
@@ -9,14 +9,14 @@ const account1 = {
     interestRate: 1.2, // %
     pin: 1111,
     transactionDates: [
-        '2019-11-18T21:31:17.178Z',
-        '2019-12-23T07:42:02.383Z',
         '2020-01-28T09:15:04.904Z',
         '2020-04-01T10:17:24.185Z',
         '2020-05-08T14:11:59.604Z',
         '2020-05-27T17:01:17.194Z',
         '2020-07-11T23:36:17.929Z',
         '2020-07-12T10:51:36.790Z',
+        '2021-02-18T21:31:17.178Z',
+        '2021-02-23T07:42:02.383Z',
     ],
     currency: 'EUR',
     locale: 'pt-PT',
@@ -108,22 +108,15 @@ const inputClosePin = document.querySelector('.form__input--pin');
 // Function: Display transactions to UI
 const displayTransactions = (account, sort = false) => {
     containerTransactions.innerHTML = '';
-
-    //
     const txns = sort
         ? account.transactions.slice().sort((a, b) => a - b)
         : account.transactions;
 
     txns.forEach((currentItem, i) => {
         const type = currentItem < 0 ? 'withdrawal' : 'deposit';
-
-        // Generate Date
         const date = new Date(account.transactionDates[i]);
-        const day = `${date.getDate()}`.padStart(2, '0');
-        const month = `${date.getMonth() + 1}`.padStart(2, '0');
-        const year = date.getFullYear();
-        const displayDate = `${day}/${month}/${year}`;
-
+        // generate date
+        const displayDate = formatTxnDate(date);
         const html = `
             <div class="transactions__row">
                 <div class="transactions__type transactions__type--${type}">${i} ${type}</div>
@@ -173,14 +166,6 @@ const createUsername = (accounts) => {
     });
 };
 
-// Function: Filter deposits
-const deposits = (transactions) =>
-    transactions.filter((transaction) => transaction > 0);
-
-// Function: Filter withdrawls
-const withdrawls = (transactions) =>
-    transactions.filter((transaction) => transaction < 0);
-
 // Function: Calculate account balance & display to UI
 const displayAccountBalance = (account) => {
     const { transactions } = account;
@@ -188,16 +173,32 @@ const displayAccountBalance = (account) => {
     labelBalance.textContent = `${account.balance.toFixed(2)}€`;
 };
 
-// FUnction: Update UI
+// Function: Update UI
 const updateUI = (account) => {
     // Display transactions
     displayTransactions(account);
-
     // Display balance
     displayAccountBalance(account);
-
     // Display summary
     calcDisplaySummary(account);
+};
+
+// Function:
+const formatTxnDate = (date) => {
+    const calcDaysPassed = (date1, date2) =>
+        Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+    const daysPassed = calcDaysPassed(new Date(), date);
+
+    // Return statements under 7days
+    if (daysPassed === 0) return 'Today';
+    if (daysPassed === 1) return 'Yesderday';
+    if (daysPassed <= 7) return `${daysPassed} days ago`;
+
+    // Return date if 7+days
+    const day = `${date.getDate()}`.padStart(2, '0');
+    const month = `${date.getMonth() + 1}`.padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
 };
 
 // Create account usernames
